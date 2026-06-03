6fab4c57a1951477326f5804f3793361f2ca830c
mirzapandiangan7 <hakimpandiangan9@gmail.com>
2026-06-03 19:20:05 +0700

    Perbaikan UI Activity Log

--- database/migrations/2026_06_03_000000_create_activity_logs_table.php
+++ database/migrations/2026_06_03_000000_create_activity_logs_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
         });
     }
 
-    public function down(): void
+    public function down(): void 
     {
         Schema::dropIfExists('activity_logs');
     }
